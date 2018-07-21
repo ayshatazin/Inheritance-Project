79a98b983d767680405b213253992731bb03d3b1
HP ProDesk <ayshataz1996@gmail.com>
2018-07-21 01:34:16 -0700

    initial1 commit

--- src/com/company/Father.java
+++ src/com/company/Father.java
@@ -2,12 +2,12 @@ package com.company;
 
 public class Father extends GrandFather {
 
-    //int numberOfCars = 5;
+    int numberOfCars = 5;
 
-    //public void says(){
+    public void says(){
 
-        //System.out.println("I'm the father");
+        System.out.println("I'm the father");
 
-   // }
+    }
 
 }
